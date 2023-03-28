76666ccb266286db8754933d7d0e8d7d347da684
Richard Matic <rmatic@stratpoint.com>
2023-03-29 00:09:55 +0800

    Create cat details page which disply the data of the cat based on cat id

---
+++ src/pages/CatDetails.tsx
@@ -0,0 +1,51 @@
+import { useParams } from 'react-router-dom';
+import { NavBar } from '../components/NavBar';
+import { Card, Container } from 'react-bootstrap';
+import { AlertBanner } from '../components/AlertBanner';
+import { CatAction } from '../contexts/CatContext';
+import UseFetch from '../hooks/useFetch';
+
+const CatDetails = (): JSX.Element => {
+  const params = useParams();
+  const {
+    loading: catDetailsLoading,
+    response: catDetailsResponse,
+    error: catDetailsError,
+  } = UseFetch({ endpoint: `/images/${params.id}`, action: CatAction.FetchCatDetails });
+
+  return (
+    <div>
+       <Container>
+        {catDetailsError
+          && <AlertBanner message='Apologies but
+          we could not load new cats for you at this time! Miau!' />}
+      </Container>
+      <NavBar />
+      <div>
+        {catDetailsLoading && 'Fetching cat details...'}
+      </div>
+  
+      {catDetailsResponse &&
+        <Card style={{ margin: '5rem auto', width: '60rem' }}>
+          <Card.Img variant="top" src={catDetailsResponse.url} />
+          <Card.Body>
+            <Card.Title>
+              Name: {catDetailsResponse.breeds && catDetailsResponse.breeds[0].name}
+            </Card.Title>
+            <Card.Text>
+              Origin: {catDetailsResponse.breeds && catDetailsResponse.breeds[0].origin}
+            </Card.Text>
+            <Card.Text>
+              Temperament: {catDetailsResponse.breeds && catDetailsResponse.breeds[0].temperament}
+            </Card.Text>
+            <Card.Text>
+              Description: {catDetailsResponse.breeds && catDetailsResponse.breeds[0].description}
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      }
+  </div>
+  );
+}
+
+export default CatDetails;
